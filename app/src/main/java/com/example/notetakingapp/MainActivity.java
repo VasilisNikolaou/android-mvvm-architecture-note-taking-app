861cbd3ba67832a32b6808cfe4c9f5b506524684
Vasilis Nikolaou <basilisnik16@gmail.com>
2021-03-02 13:19:40 +0200

    Added add note functionality

--- app/src/main/java/com/example/notetakingapp/MainActivity.java
+++ app/src/main/java/com/example/notetakingapp/MainActivity.java
@@ -1,17 +1,24 @@
-    package com.example.notetakingapp;
+package com.example.notetakingapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.notetakingapp.adapter.NoteAdapter;
+import com.example.notetakingapp.model.Note;
 import com.example.notetakingapp.viewmodel.NoteViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int ADD_NOTE_REQUEST = 1;
 
     private NoteViewModel noteViewModel;
 
@@ -20,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Notes");
+
+        final FloatingActionButton fab = findViewById(R.id.button_add_note);
+        fab.setOnClickListener((View v) -> {
+            Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+            startActivityForResult(intent, ADD_NOTE_REQUEST);
+        });
 
         final NoteAdapter noteAdapter = new NoteAdapter();
         final RecyclerView recyclerView = findViewById(R.id.recycler_view);
@@ -30,5 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
         this.noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         this.noteViewModel.getNotes().observe(this, noteAdapter::setNotes);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY, 1);
+
+            final Note note = new Note(title, description, priority);
+            noteViewModel.insertNote(note);
+
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
